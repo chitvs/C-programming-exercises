b1547237d6f940697cba81c3e3464e15d1d04349
chitvs <140112899+chitvs@users.noreply.github.com>
2024-06-01 19:39:00 +0200

    Create aux.h

---
+++ Exam/16-06-2020/Exercise 2/aux.h
@@ -0,0 +1,29 @@
+#pragma once
+#include <stdio.h>
+#include <stdlib.h>
+
+#ifndef SCL_H
+#define SCL_H
+
+typedef int TipoInfoSCL;
+
+struct TipoNodoSCL{
+	TipoInfoSCL info;
+	struct TipoNodoSCL* next;
+};
+
+typedef TipoNodoSCL* TipoSCL;
+
+// prints a list to stdout
+void print(TipoSCL list_);
+TipoSCL generate(const int& number_of_nodes_);
+TipoSCL copySCL(TipoSCL src);
+void freeSCL(TipoSCL list_);
+TipoSCL generateFromArray(TipoInfoSCL *arr, int len);
+
+#endif
+int sommaLista(TipoSCL l, int i, int j);
+TipoSCL sommaPalindroma(TipoSCL l);
+
+int contaDecrescenti(TipoSCL lis);
+int mergeSequenziale(TipoSCL lis1, TipoSCL lis2);
