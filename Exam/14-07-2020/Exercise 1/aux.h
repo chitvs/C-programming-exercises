fd25fac6d994cea676f0076adfe2945f31682b54
chitvs <140112899+chitvs@users.noreply.github.com>
2024-06-01 19:37:20 +0200

    Update aux.h

--- Exam/14-07-2020/Exercise 1/aux.h
+++ Exam/14-07-2020/Exercise 1/aux.h
@@ -1,24 +1,51 @@
-#pragma once
-#include <stdio.h>
-#include <stdlib.h>
+#include "aux.h"
 
-#ifndef MAT_H
-#define MAT_H
+Mat *allocMatrix(int rows, int cols){
+  Mat *m = (Mat*)malloc(sizeof(Mat));
+  m->rows = rows;
+  m->cols = cols;
+  m->row_ptrs = (int **)malloc(sizeof(int*)*rows);
+  for(int r = 0; r < rows; r++)
+    m->row_ptrs[r] = (int *)malloc(sizeof(int)*cols);
+  return m;
+}
 
-typedef int TipoInfoMat;
+void freeMatrix(Mat* src){
+  if( src == NULL )
+    return;
+  int rows = src->rows;
+  //  int cols = src->cols;
+  for(int r=0; r<rows; ++r)
+    free(src->row_ptrs[r]);
+  free(src);
+}
 
-typedef struct {
-  int rows;
-  int cols;
-  TipoInfoMat **row_ptrs;
-} Mat;
+Mat *randomMat( int rows, int cols, int max ){
+  Mat *m = allocMatrix(rows, cols);
+  max++;
+  for( int r = 0; r < rows; r++ )
+    for( int c = 0; c < cols; c++ )
+      m->row_ptrs[r][c] = rand()%max;
+  return m;
+}
 
-Mat *allocMatrix(int rows, int cols);
-void printMatrix(Mat* src);
-void freeMatrix(Mat* src);
-Mat* randomMat(int rows, int cols, int max);
-int matEqual(Mat * m1, Mat * m2);
-Mat * copyMat(Mat *m);
-#endif
+Mat *uniformMat( int rows, int cols, int val ){
+  Mat *m = allocMatrix(rows, cols);
+  for( int r = 0; r < rows; r++ )
+    for( int c = 0; c < cols; c++ )
+      m->row_ptrs[r][c] = val;
+  return m;
+}  
 
-void applicaFiltro(Mat * src, Mat * filtro, int riga, int colonna);
+void printMatrix(Mat* src){
+  if(src == NULL){
+    printf("NULL\n");
+    return;}
+  int rows = src->rows;
+  int cols = src->cols;
+  for(int r=0; r<rows; ++r) {
+    for(int c=0; c<cols; ++c) {
+      printf("%d\t", src->row_ptrs[r][c]);}
+    printf("\n");}
+  fflush(stdout);
+}
