a294853505739de28536008b200e38b64111ca77
chitvs <140112899+chitvs@users.noreply.github.com>
2024-06-01 18:23:29 +0200

    Update aux.h

--- Exam/13-06-2023/D/Exercise 1/aux.h
+++ Exam/13-06-2023/D/Exercise 1/aux.h
@@ -1,6 +1,5 @@
 #pragma once
 
-
 // Questa è la struttura Mat
 typedef struct {
   int rows;
@@ -8,12 +7,10 @@ typedef struct {
   int **mat;
 } Mat;
 
-
 /* Funzioni a disposizione dello studente */
 Mat * mat_alloc(int rows, int cols); // Alloca una matrice
 void mat_print(const Mat *m);        // Stampa una matrice
 void mat_free(Mat *m);               // Dealloca una matrice
-
 
 // Dichiarazione della funzione da scrivere
 Mat* ultimi_gli_unici(Mat *m);
