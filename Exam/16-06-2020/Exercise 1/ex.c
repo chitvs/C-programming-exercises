614d46bf668277c4c5ba948a1c65c9493e1a9a86
chitvs <140112899+chitvs@users.noreply.github.com>
2024-06-01 19:32:12 +0200

    Create ex.c

---
+++ Exam/16-06-2020/Exercise 1/ex.c
@@ -0,0 +1,11 @@
+#include "aux.h"
+
+void bombardamentoNavale(Mat *src, int r, int c){
+  if(r < 0 || r >= src->rows || c < 0 || c >= src->cols || src->row_ptrs[r][c] == 0){
+    return;}
+  src->row_ptrs[r][c] = 0; 
+  bombardamentoNavale(src, r+1, c);
+  bombardamentoNavale(src, r-1, c);
+  bombardamentoNavale(src, r, c+1);
+  bombardamentoNavale(src, r, c-1);
+}
