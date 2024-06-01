a1efb0156e44260842d1b073130bb302d0589f26
chitvs <140112899+chitvs@users.noreply.github.com>
2024-06-01 13:53:52 +0200

    Update aux.c

--- Exam/11-09-2023/Exercise 2/aux.c
+++ Exam/11-09-2023/Exercise 2/aux.c
@@ -30,42 +30,42 @@ TipoLista cdr(TipoLista l){
 }
 
 void printlist_aux(TipoLista l){
-	if (estVuota(l)) return;
-	printf("%d, ", car(l));
-	printlist_aux(cdr(l));
+  if (estVuota(l)) return;
+  printf("%d, ", car(l));
+  printlist_aux(cdr(l));
 }
 
 void printlist(TipoLista l){
-	if (estVuota(l))
-		printf("()");
-	else
-		printlist_aux(l);
-	printf("\n");
+  if (estVuota(l))
+    printf("()");
+  else
+    printlist_aux(l);
+  printf("\n");
 }
 
 void freelist(TipoLista l){
-	if (l == NULL)
-		return;
-	freelist(l->next);
-	free(l);
+  if (l == NULL)
+    return;
+  freelist(l->next);
+  free(l);
 }
 
 int length(TipoLista l){
-	if (estVuota(l)) 
-		return 0;
-	return 1 + length(cdr(l));
+  if (estVuota(l)) 
+    return 0;
+  return 1 + length(cdr(l));
 }
 
 int sommaLista_aux(int somma, TipoLista l){
-	if (estVuota(l))
-		return somma;
-	else
-		return somma + sommaLista_aux(car(l), cdr(l));
+  if (estVuota(l))
+    return somma;
+  else
+    return somma + sommaLista_aux(car(l), cdr(l));
 }
 
 int sommaLista(TipoLista l){
-	if (estVuota(l))
-		return 0;
-	else	
-		return sommaLista_aux(car(l), cdr(l));
+  if (estVuota(l))
+    return 0;
+  else	
+    return sommaLista_aux(car(l), cdr(l));
 }
