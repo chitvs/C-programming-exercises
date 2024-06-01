f2c14b9a49d2dc5e38bb0960ab2d440d9bea5b20
chitvs <140112899+chitvs@users.noreply.github.com>
2024-06-01 16:57:37 +0200

    Update aux.h

--- Exam/11-09-2023/Exercise 3/aux.h
+++ Exam/11-09-2023/Exercise 3/aux.h
@@ -2,8 +2,10 @@
 
 #include <stdbool.h>
 
+// Tipo info dell'albero
 typedef int TipoInfoAlbero;
 
+// Albero con strutture collegate
 typedef struct StructAlbero {
   TipoInfoAlbero info;
   struct StructAlbero* destro;
@@ -12,13 +14,18 @@ typedef struct StructAlbero {
 
 typedef TipoNodoAlbero* TipoAlbero;
 
+// Funzioni tipo astratto albero binario
 TipoAlbero albBinVuoto();
 TipoAlbero creaAlbBin(TipoInfoAlbero info, TipoAlbero sx, TipoAlbero dx);
 bool estVuoto(TipoAlbero a);
 TipoInfoAlbero radice(TipoAlbero a);
 TipoAlbero sinistro(TipoAlbero a);
 TipoAlbero destro(TipoAlbero a);
+
+// Funzioni esterne
 void stampaInfoAlbero(TipoInfoAlbero c);
 void stampaParentetica(TipoAlbero a);
 void stampaAlbero(TipoAlbero a);
+
+/** Esercizio da svolgere **/
 void diffMinFogliaMaxAntenato(TipoAlbero a);
